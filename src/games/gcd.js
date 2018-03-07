cfee0f5aa6178c45fbaa0f518b4392a2c47b95ba
gabos31 <exbt@yandex.ru>
2018-03-07 23:43:13 +0100

    part 6

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,19 +1,31 @@
-export const task = 'Find the greatest common divisor of given numbers.';
-const a = 0;
-export const n = () => {
-  let f = '';
-  const x = Math.floor(Math.random() * 100);
-  const y = Math.floor(Math.random() * 100);
-  f = `${x} ${y}`;
-  let n = 0;
-  let m = 0;
-  if (x < y) n = x;
-  else n = y;
-  for (let i = n; i !== 1; i -= 1) {
+import * as index from '..';
 
-  }
-  return f;
-};
-export const check = () => {
-
+export default () => {
+  const task = 'Find the greatest common divisor of given numbers.';
+  index.brain(task);
+  let a = 0;
+  const que = () => {
+    let f = '';
+    const x = Math.floor(Math.random() * 100);
+    const y = Math.floor(Math.random() * 100);
+    f = `${x} ${y}`;
+    let n = 0;
+    let m = 0;
+    if (x < y) {
+      n = x;
+      m = y;
+    } else {
+      n = y;
+      m = x;
+    }
+    for (let i = n; i > 0; i -= 1) {
+      if (n % i === 0 && m % i === 0) {
+        a = i;
+        break;
+      }
+    }
+    return f;
+  };
+  const ans = () => String(a);
+  index.game(que, ans);
 };
