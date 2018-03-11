f28982ff65e2cd9765450856acd78f18bc81b0a5
gabos31 <exbt@yandex.ru>
2018-03-11 17:38:55 +0100

    part 7 correct 7

--- src/games/balance.js
+++ src/games/balance.js
@@ -8,11 +8,9 @@ export default () => {
     const question = String(makeRandom(0, 5000));
     const makeAnswer = (arr) => {
       arr.sort((a, b) => a - b);
-      const firstDigit = arr[0];
-      const lastDigit = arr[arr.length - 1];
-      if (isBalance(lastDigit, firstDigit) || arr.length === 1) return arr.join('');
-      arr.splice(arr.length - 1, 1, lastDigit - 1);
-      arr.splice(0, 1, firstDigit + 1);
+      if (isBalance(arr[arr.length - 1], arr[0]) || arr.length === 1) return arr.join('');
+      arr.splice(arr.length - 1, 1, arr[arr.length - 1] - 1);
+      arr.splice(0, 1, arr[0] + 1);
       return makeAnswer(arr);
     };
     return cons(question, makeAnswer(question.split('').map(Number)));
