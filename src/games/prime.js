615de571a10cc46e497bf7b611beccd9c905f8a1
gabos31 <exbt@yandex.ru>
2018-03-12 10:37:00 +0100

    part 9 correct 2

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,12 +7,12 @@ export default () => {
   const makeRound = () => {
     const question = makeRandom(2, 100);
     const isPrime = (x) => {
-      const hasDevisor = (i) => {
+      const hasDivisor = (i) => {
         if (i > x / 2) return false;
         if (x % i === 0) return true;
-        return hasDevisor(i + 1);
+        return hasDivisor(i + 1);
       };
-      if (!hasDevisor(2)) return true;
+      if (!hasDivisor(2)) return true;
       return false;
     };
     return cons(question, (isPrime(question)) ? 'yes' : 'no');
